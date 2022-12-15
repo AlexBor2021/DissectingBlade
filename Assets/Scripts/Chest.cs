37fdce2847e1d9f33291d088243e5720e1645fd3
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2022-12-15 12:25:00 +0300

    работа над ошибками

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private Pointer _pointer;
 
     private const string _open = "Open";
+    private bool _isOpen;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<LimbPlayer>())
+        if (other.GetComponent<LimbPlayer>() && _isOpen == false)
         {
             _animator.SetTrigger(_open);
+            _pointer.DeletePointer();
+            _isOpen = true;
         }
     }
 }
