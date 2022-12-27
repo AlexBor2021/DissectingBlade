fd9f47047635986f779ddb02a9c62fa72a55ff13
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2022-12-27 18:12:00 +0300

    Актуалочка для всех!!!!!!!!!!!

--- Assets/Scripts/UI/FinishMenu/FinishIcon.cs
+++ Assets/Scripts/UI/FinishMenu/FinishIcon.cs
@@ -13,14 +13,13 @@ public class FinishIcon : MonoBehaviour
     [SerializeField] private GameObject _bossIcon;
     [SerializeField] private List<Animator> _animatorsStar;
     [SerializeField] private PauseGame _pauseGame;
+    [SerializeField] private int _numberLevel;
 
-    private int _numberLevel;
     private int _numberSceneLevelMenu = 2;
     private int _comliteLevel = 1;
 
     private void OnEnable()
     {
-        _numberLevel = SceneManager.GetActiveScene().buildIndex-2;
         _pauseGame.SetPause();
     }
     private void OnDisable()
@@ -46,7 +45,7 @@ public class FinishIcon : MonoBehaviour
         _coinCount.text = "x " + revard.ToString();
 
         SetStars(countStars);
-        
+
         ManagerInfoGame.SaveInfoLevel(_numberLevel, countStars, _comliteLevel);
         ManagerInfoGame.AddCoinInWalletPlayer(revard);
     }
--- Assets/Scripts/UI/Menu/MenuSettings.cs
+++ Assets/Scripts/UI/Menu/MenuSettings.cs
@@ -6,6 +6,7 @@ public class MenuSettings : MonoBehaviour
     [SerializeField] private PauseGame _pauseGame;
 
     private int _numberSceneMainMenu = 1;
+    private int _numberSceneLevelMenu = 2;
     private void OnEnable()
     {
         _pauseGame.SetPause();
@@ -17,6 +18,10 @@ public class MenuSettings : MonoBehaviour
     public void LoadMainMenu()
     {
         SceneManager.LoadScene(_numberSceneMainMenu);
+    }
+    public void LoadLevelMenu()
+    {
+        SceneManager.LoadScene(_numberSceneLevelMenu);
     }
     public void Restart()
     {
