b09743fb4bb09547fdd143082810c9c2e6f8cab5
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2022-12-28 09:24:33 +0300

    1

--- Assets/Scripts/UI/Shop/WeaponShop.cs
+++ Assets/Scripts/UI/Shop/WeaponShop.cs
@@ -42,26 +42,34 @@ public class WeaponShop : MonoBehaviour
 
     private void LoadInfo()
     {
-        if (PlayerPrefs.GetInt(ManagerInfoGame.WeaponShopInfo.IsBuyWeapon) > 0)
+        if (_indexWepon == 0)
         {
-            _isBuy = true;
             _buttonBuy.SetActive(false);
-
-            if (PlayerPrefs.GetInt(ManagerInfoGame.WeaponShopInfo.IsSetWeapon) > 0)
-            {
-                _isSetWeapon = true;
-                _buttonSet.SetActive(false);
-            }
-            else
-            {
-                _isSetWeapon = false;
-                _buttonSet.SetActive(true);
-            }
+            _buttonSet.SetActive(false);
         }
         else
         {
-            _isBuy = false;
-            _buttonBuy.SetActive(true);
+            if (PlayerPrefs.GetInt(ManagerInfoGame.WeaponShopInfo.IsBuyWeapon) > 0)
+            {
+                _isBuy = true;
+                _buttonBuy.SetActive(false);
+
+                if (PlayerPrefs.GetInt(ManagerInfoGame.WeaponShopInfo.IsSetWeapon) > 0)
+                {
+                    _isSetWeapon = true;
+                    _buttonSet.SetActive(false);
+                }
+                else
+                {
+                    _isSetWeapon = false;
+                    _buttonSet.SetActive(true);
+                }
+            }
+            else
+            {
+                _isBuy = false;
+                _buttonBuy.SetActive(true);
+            }
         }
     }
 }
