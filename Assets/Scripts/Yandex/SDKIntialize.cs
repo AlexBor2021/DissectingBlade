0506a1723e83d2df7a1a436504bce7b73fb65499
Алексей <93997737+AlexBor2021@users.noreply.github.com>
2022-12-29 18:40:38 +0300

    no message

--- Assets/Scripts/Yandex/SDKIntialize.cs
+++ Assets/Scripts/Yandex/SDKIntialize.cs
@@ -9,12 +9,6 @@ using UnityEngine.SceneManagement;
 
 public class SDKIntialize : MonoBehaviour
 {
-    private BasaData _basaData;
-    public BasaData BaseData => _basaData;
-    
-    public bool InputSystemKeyBoard;
-
-
     private IEnumerator  Start()
     {
          DontDestroyOnLoad(this.gameObject);
@@ -22,44 +16,10 @@ public class SDKIntialize : MonoBehaviour
 #if UNITY_WEBGL && YANDEX_GAMES && !UNITY_EDITOR
 
         yield return YandexGamesSdk.Initialize();
-
-        PlayerAccount.GetPlayerData(OnInitialized, OnError);
-
-        void OnInitialized(string data)
-        {
-            Debug.Log("Initialized");
-            _basaData = JsonUtility.FromJson<BasaData>(data);
-            Debug.Log(_basaData + " загрузилось сохранение!!!");
-            SceneManager.LoadScene(1);
-        }
-
-        void OnError(string error)
-        {
-            Debug.Log(error);
-            _basaData = new BasaData();
-            Debug.Log(" создалось сохранение!!!");
-            SceneManager.LoadScene(1);
-        }
-#elif UNITY_WEBGL && VK_GAMES && !UNITY_EDITOR
-
-        yield return VKGamesSdk.Initialize();
-        SceneManager.LoadScene(1);
-        Debug.Log("загрузка осноговного меню");
 #else
-        _basaData = new BasaData();
         SceneManager.LoadScene(1);
 #endif
         yield return null;
-
-        InputSystemKeyBoard = SetInputGameKeyBoard();
-        
-    }
-    public bool SetInputGameKeyBoard()
-    {
-#if UNITY_WEBGL && YANDEX_GAMES && !UNITY_EDITOR
-        return Device.Type == Agava.YandexGames.DeviceType.Desktop;
-#endif
-        return SystemInfo.deviceType ==  UnityEngine.DeviceType.Desktop;
     }
 
 }
